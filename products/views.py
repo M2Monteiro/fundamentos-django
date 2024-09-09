2a962fbf31e16dbd164dc9d1e27be9d3dfdd575d
Matheus <matheus.stack.20@hotmail.com>
2024-09-09 13:15:44 -0300

    Página de Detalhes

--- products/views.py
+++ products/views.py
@@ -4,8 +4,9 @@ from .models import Product
 
 def products_list(request):
     products = Product.objects.all().order_by("created_at")
-    return render(
-        request,
-        "products/products_list.html",
-        {'products': products}
-    )
+    return render(request, "products/products_list.html", {"products": products})
+
+
+def product_page(request, slug):
+    product = Product.objects.get(slug=slug)
+    return render(request, "products/product_page.html", {"product": product})
