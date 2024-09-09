3a51e7b06bc885299a9b53e09323522f59fb6cd9
Matheus <matheus.stack.20@hotmail.com>
2024-09-09 12:13:01 -0300

    URL Nomeada

--- products/views.py
+++ products/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from .models import Product
 
 
-# Create your views here.
 def products_list(request):
-    return render(request, "products/products_list.html")
+    products = Product.objects.all().order_by("created_at")
+    return render(
+        request,
+        "products/products_list.html",
+        {'products': products}
+    )
