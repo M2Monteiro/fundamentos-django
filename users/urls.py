29f90398b470672fd05dc491e1de0f06c8ecdc88
Matheus <matheus.stack.20@hotmail.com>
2024-09-11 11:27:57 -0300

    Logout

--- users/urls.py
+++ users/urls.py
@@ -6,4 +6,5 @@ app_name = "users"
 urlpatterns = [
     path("register", views.register_view, name="register"),  # users:register
     path("login", views.login_view, name="login"),  # users:login
+    path('logout', views.logout_view, name="logout"), #users:logout
 ]
--- users/views.py
+++ users/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth import login
+from django.contrib.auth import login, logout
 
 
 def register_view(request):
@@ -23,3 +23,9 @@ def login_view(request):
     else:
         form = AuthenticationForm()
     return render(request, "users/login.html", {"form": form})
+
+
+def logout_view(request):
+    if request.method == "POST":
+        logout(request)
+        return redirect("products:list")
